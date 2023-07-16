aa8b835bd6a5e3e9256c8edfe69fb89aef4a869e
Ismael Laet <ismaellaet1@gmail.com>
2023-07-16 17:45:41 -0300

    feat: Customize errors response

---
+++ src/main/java/med/voll/api/infra/CustomExceptionHandler.java
@@ -0,0 +1,30 @@
+package med.voll.api.infra;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
+
+@RestControllerAdvice
+public class CustomExceptionHandler {
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity handleNotFoundException() {
+        return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity handleNotValidException(MethodArgumentNotValidException ex) {
+        List<FieldError> fieldErrors = ex.getFieldErrors();
+        return ResponseEntity.badRequest().body(fieldErrors.stream().map(NotValidErrorResponse::new).toList());
+    }
+
+    private record NotValidErrorResponse(String field, String message) {
+        public NotValidErrorResponse(FieldError error) {
+            this(error.getField(), error.getDefaultMessage());
+        }
+    }
+}
